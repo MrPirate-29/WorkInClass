359aab38316b78331377f1426b52bd4a0a84a27f
Jhoel Herrera <corderoimisho@gmail.com>
2026-09-24 16:28:35 -0400

    Refinamiento de animaciones de correr y saltar

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -24,6 +24,7 @@ public class Jugador : MonoBehaviour
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, alturaSalto);
         animator.SetFloat("Velocidad", Mathf.Abs(movimiento));
         animator.SetFloat("VelocidadVertical", rb.linearVelocity.y);
+        animator.SetBool("estaEnPiso", esPiso);
     }
     public void FixedUpdate()
     {
